915204648e990208ece4087ac40a78caa507d5e5
Subash Jirel <subashjrl@gmail.com>
2022-08-03 15:55:21 +0545

    artists api

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//new one
+
+document.querySelector('button').addEventListener('click',run)
+
+const run = ()=> {
+ artistName = document.querySelector('input').value   
+}
+alert('working')
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const artists = {
+    theweeknd : {
+        age:29,
+        name: 'Abel Tesfaye',
+        birthLocation: 'Canada'
+    },
+
+    jojo : {
+        age:21,
+        name: 'Giornia jouvan',
+        birthLocation: 'Italy'
+    },
+
+    goku : {
+        age:35,
+        name: 'Son Goku"',
+        birthLocation: 'Outer Space'
+    },
+    '21 dylan' : {
+        age:'dylan',
+        name: 'dylan"',
+        birthLocation: 'dylan'
+
+    }
+}
+
+app.get('/', (request,response)=>{
+    response.sendFile(__dirname+'/index.html')
+})
+
+app.listen(8000,()=> {
+    console.log(`server is running on port ${PORT}`)
+})
+
+//creating the api
+app.get('/api/:artistName',(request,response)=> { // the colons ':' lets us know that its a queryparam & not filepath.
+   // console.log(request.params.artistName)
+   const artistName = request.params.artistName.toLowerCase()
+
+   if(artists[artistName]) {   // we use bracket notation because some property has spaces in the name//truthy huda run
+    response.json(artists[artistName]) //if the artists name in query param exist in the js obj . the info is displayed
+
+   }
+   else {
+    response.json(artists['21 dylan'])
+   }
+    //response.json(artists)
+})
+
